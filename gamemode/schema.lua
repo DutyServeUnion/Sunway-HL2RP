91c8b547f333ea5e89faa8a7b6ae6e227776d676
DutyServeUnion <84892309+DutyServeUnion@users.noreply.github.com>
2021-10-16 02:44:01 +0400

    admin+schema

---
+++ gamemode/schema.lua
@@ -0,0 +1,30 @@
+CAKE.Schemas = {  };
+CAKE.Schemafile = {  };
+
+function CAKE.LoadSchema( schema )
+
+	local path = "schemas/" .. schema .. ".lua"
+	
+	SCHEMA = {  }
+	
+	include( path )
+	table.insert( CAKE.Schemas, SCHEMA )
+
+	-- Load the base, first.
+
+	if( SCHEMA.Base ) then
+	
+		CAKE.LoadSchema( SCHEMA.Base )
+		
+	end
+	
+	table.insert( CAKE.Schemafile, schema )
+	CAKE.DayLog( "script.txt", "Loading schema " .. SCHEMA.Name .. " by " .. SCHEMA.Author .. " ( " .. SCHEMA.Description .. " )" );
+
+
+	local list = file.FindDir( GM.Folder .. "/gamemode/schemas/" .. schema .. "/plugins/*", true  )
+
+	for k, v in pairs( list ) do
+
+		if v != "." and v != ".." then
+			CAKE.LoadPlugin( schema, v )

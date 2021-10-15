7401910ec67e70ebb4825ab0aef99e777dbe8158
Andrew (Minty) <84892309+DutyServeUnion@users.noreply.github.com>
2021-10-16 02:34:14 +0400

    Update cl_init.lua

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -32,3 +32,10 @@ CAKE.UseIntro = true --Set this to false if you want the player to go directly i
 CAKE.IntroText = "N/A" -- Character menu and intro text. NOTE, the HL2RP scheme changes this
 CAKE.IntroSubtitle = "N/A" -- Character menu and intro subtitle. If you want this gone just set it to ""
 CAKE.ChatFont = "ChatFont" -- Main font used in chatting
+
+surface.CreateFont(CAKE.MenuFont, 32, 500, true, false, "TitlesFont", false, true) -- Biggest font used. Used in 3D titles and main character title.
+surface.CreateFont(CAKE.MenuFont, 18, 500, true, false, "TimeFont", true, false ) -- Second biggest used. 
+surface.CreateFont(CAKE.MenuFont, 14, 500, true, false, "SubtitlesFont", false, true) -- A moderate size font used for the main title's subtitle
+surface.CreateFont(CAKE.MenuFont, 12, 400, true, false, "TabsFont", true ) -- Smallest, used in tabs and the quick menu
+surface.CreateFont("DefaultSmallDropShadow", ScreenScale(5), 500, true, false, "WhisperFont", true ) -- Used only for whispering
+surface.CreateFont("Trebuchet18", ScreenScale(10), 700, true, false, "YellFont", true ) -- Used only for yelling
